545c899fb682c82e2bd21d047a747e38df327a83
Peirannnn <sarahprwang@gmail.com>
2021-10-29 23:05:26 -0500

    get data from backend

--- frontend/src/views/Communities.vue
+++ frontend/src/views/Communities.vue
@@ -10,8 +10,9 @@
         her
       </infoPage>
     </v-overlay>
+    <!-- search list-->
     <div
-      class="search-list-container mx-16"
+      class="search-list-container mx-16 mt-1"
       :style="{ width: searchWidth, left: searchPositionX, top: searchPositionY }"
       v-if="items.length && !lose"
     >
@@ -77,6 +78,8 @@
         </v-text-field>
       </div>
     </div>
+    
+    <!-- search result cards-->
     <div style="font-size:20px;font-weight: bold;" class="mt-5" v-show="!inputLoad">{{cardAbout}}</div>
     <div v-if="!inputLoad">
     <v-row dense>
@@ -113,6 +116,7 @@
 <script>
 import CommunitiesCard from "../components/CommunitiesCard.vue"
 import infoPage from "../components/MoreInfoPage.vue"
+import { mapState, mapActions, mapGetters } from "vuex";
 export default {
   components:{
     CommunitiesCard,
@@ -157,103 +161,6 @@ export default {
         memberCount: 366,
       },
       ],
-      info: [{
-        communityId: 0,
-        name: "keep",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window. Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 1,
-        name: "must",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 2,
-        name: "behind",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 3,
-        name: "girl",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 4,
-        name: "case",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 5,
-        name: "under",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 6,
-        name: "party",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 7,
-        name: "participant",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 8,
-        name: "team",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 9,
-        name: "south",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 10,
-        name: "watch",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      },
-      {
-        communityId: 11,
-        name: "firm",
-        description: "Never range practice mind upon school worry ball. Statement pull hundred important.That production suddenly than husband list. Reality again talk save sit Mrs computer.Paper example single be over. Team show interview window.",
-        createdAt: 123073884967844,
-        photo: "",
-        memberCount: 366,
-      }
-      ],
       index: 0,
       lose: false,
       input: "",
@@ -265,9 +172,13 @@ export default {
       searchPositionY: 0,
     };
   },
+  computed:{
+    ...mapGetters(["searchResult"]),
+  },
   watch: {
     input(val) {
-      val && val !== this.select && this.querySelections(val);
+      this.searchResultTest(val)
+      this.querySelections()
       this.lose = false;
       if (val == ""){
         this.items = []
@@ -275,6 +186,10 @@ export default {
     },
   },
   methods: {
+    ...mapActions(["getSearchResult"]),
+    async searchResultTest(input){
+      await this.getSearchResult(input);
+    },
     showMoreInfo(info){
       this.moreInfo = info
       console.log(this.moreInfo)
@@ -319,19 +234,16 @@ export default {
       this.searchPositionX = left - 64 + "px";
       this.searchPositionY = top + 50 + "px";
     },
-    querySelections(v) {
+    querySelections() {
       this.loading = true;
       // Simulated ajax query
       setTimeout(() => {
-        this.items = this.info.filter((obj) => {
-          return (obj.name || "").toLowerCase().indexOf((v || "").toLowerCase()) > -1;
-        })
-        
+        this.items = this.searchResult
         if (this.items.length > 10) {
           this.items = this.items.slice(0, 10);
         }
         this.loading = false;
-      }, 500);
+      }, 1000);
     },
   },
   mounted() {
