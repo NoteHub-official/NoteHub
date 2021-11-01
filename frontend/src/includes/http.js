1cdc4157a00a0827b4ccdfc6a99b78bf38976e24
Toubat <57741529+Toubat@users.noreply.github.com>
2021-11-01 01:41:34 -0500

    update dotenv

--- frontend/src/includes/http.js
+++ frontend/src/includes/http.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env);
 
 const http = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: process.env.VUE_APP_ROOT_URL,
   headers: {
     "Content-type": "application/json",
     "CSRF-Token": Cookies.get("XSRF-TOKEN"),
