ce65b41ce3e4f2ddea0b3ce46846ebe041687784
Lucheng_Hugh <hughqing@qq.com>
2021-11-21 15:25:50 -0600

    fix a dashboard not find bug

--- server/src/app.js
+++ server/src/app.js
@@ -61,8 +61,8 @@ app.all("*", (req, res, next) => {
 app.use("/api", apiRouter);
 
 // intent to delete
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = { app };
