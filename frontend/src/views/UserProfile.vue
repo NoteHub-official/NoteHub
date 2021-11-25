88024db8069defdb977144013b047894c9162676
Peirannnn <sarahprwang@gmail.com>
2021-11-24 18:29:04 -0600

    basic user level view in profile

--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -1,9 +1,45 @@
 <template>
-  <h1>User Profile</h1>
+  <div>
+    <v-card style="width: 100%">
+      <v-img src="../assets/profile-background.png">
+        <UserAvatar
+        :firstname="user.firstname"
+        :lastname="user.lastname"
+        :avatarUrl="user.avatarUrl"
+        :size="50"
+        />
+        <p>{{ `${user.firstname} ${user.lastname}` }}</p>
+        <p>{{ currentLevel }}</p>
+      </v-img>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {};
+import UserAvatar from "../components/UserAvatar.vue";
+import { mapActions, mapGetters} from "vuex";
+export default {
+  components: {
+    UserAvatar
+  },
+  computed: {
+    ...mapGetters(['currentLevel']),
+    user() {
+      return this.$store.state.auth.currentUser;
+    },
+  },
+  data(){
+    return {
+      //userLevel: 0
+    }
+  },
+  methods: {
+    ...mapActions(['getLevel'])
+  },
+  mounted(){
+    this.getLevel(this.$store.state.auth.currentUser.userId)
+  }
+};
 </script>
 
 <style></style>
